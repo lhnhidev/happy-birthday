3439a5b8a69c84e35ec4f176f7a9112f8a9cedb6
Nhi-Le <lhnhi420@gmail.com>
2024-07-22 04:17:56 +0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var volumn = document.querySelector("#volumn");
 var img = document.querySelector("#newImage"); 
 var ma = document.querySelector(".ma");
+var box = document.querySelector(".box");
+var title = document.querySelector("h2");
+
+box.addEventListener('click', () => {
+    box.classList.add('none');  
+    title.classList.remove('none');
+});
 
 var music = new Audio('./assets/img/audio.mp4');
 
 volumn.addEventListener('input', () => {
-    if (volumn.value < 30) {
+    if (volumn.value < 42) { // if you ask myseflt, why fucking man use 42?? Ok my answer: because i like it =))
         music.play();
         setTimeout(() => {
             ma.classList.add('show');
