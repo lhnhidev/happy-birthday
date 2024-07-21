3d4ea7ba5dfa52fe2e923329d747846e0a50b989
Nhi-Le <lhnhi420@gmail.com>
2024-07-22 03:30:55 +0700

    creat boss gift =))

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var volumn = document.querySelector("#volumn");
 var img = document.querySelector("#newImage"); 
+var ma = document.querySelector(".ma");
+
+var music = new Audio('./assets/img/audio.mp4');
 
 volumn.addEventListener('input', () => {
+    if (volumn.value < 30) {
+        music.play();
+        setTimeout(() => {
+            ma.classList.add('show');
+            newImage.classList.add('ma');        
+        }, 300);
+    }
     img.style.clipPath = "polygon(0 0, " + volumn.value + "% 0, " + volumn.value + "% 100%, 0 100%";
 });
